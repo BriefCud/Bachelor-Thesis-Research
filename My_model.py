549967067608e07e9bf314e3f9f57c73308f57a5
Alexander Leonidas <alexander@leonidas.org>
2022-05-25 13:45:29 +0200

    Update My_model.py

--- My_model.py
+++ My_model.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import pennylane as qml
 import matplotlib.pyplot as plt
-from dataset16 import load_dataset as ld_ful
+from dataset16 import load_dataset as ld_full
 from dataset_muon import load_dataset as ld_muon
 from functools import partial
 import jax
